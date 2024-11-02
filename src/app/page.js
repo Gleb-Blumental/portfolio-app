431fc9f654e315644b620fce92c5b4086fa3c459
Gle_Blu <127569304+Mepuzan@users.noreply.github.com>
2024-11-02 16:17:18 +0100

    New pages, added express package

--- src/app/page.js
+++ src/app/page.js
@@ -15,9 +15,9 @@ export default function Home() {
         />
         <ol>
           <li>
-            Get started by editing <code>src/app/page.js</code>.
+            This is my portfolio page.
           </li>
-          <li>Save and see your changes instantly.</li>
+          <li>Built with Next.js</li>
         </ol>
 
         <div className={styles.ctas}>
