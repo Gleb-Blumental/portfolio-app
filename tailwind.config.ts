7cea697e6b85aa59b889c4e5212b97dd81ea66eb
Gle_Blu <127569304+Mepuzan@users.noreply.github.com>
2024-11-02 18:48:37 +0100

    Update tailwind.config.ts

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,21 +1,27 @@
-import type { Config } from "tailwindcss";
+/*@type {import('tailwindcss').Config} */
 
-const config: Config = {
-  content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
-  ],
+
+module.exports = {
+  content: ["./src/**/*.{ts,tsx}"],
+  darkMode: "class",
   theme: {
     extend: {
-      colors : {
-        black: {
-          default: '#000',
-          100: '#000319',
-        }
-      }
+      animation: {
+        spotlight: "spotlight 2s ease .75s 1 forwards",
+      },
+      keyframes: {
+        spotlight: {
+          "0%": {
+            opacity: 0,
+            transform: "translate(-72%, -62%) scale(0.5)",
+          },
+          "100%": {
+            opacity: 1,
+            transform: "translate(-50%,-40%) scale(1)",
+          },
+        },
       },
     },
+  },
   plugins: [],
 };
-export default config;
